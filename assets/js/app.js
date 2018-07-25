7b2876a0f7ac54a5f44181a687b097e1f952787b
Elizabeth Vega M <eb.vega.m@gmail.com>
2018-07-25 19:27:51 -0400

    agregado mapa que muestra los restaurantes disponibles en la zona, cada uno con un marcador con información de nombre y dirección, además avance en el splash screen de inicio

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,23 @@
 // Fadeout de la página principal
+window.onload = () => {
+  ready();
+};
+
+function ready() {
+  const preload = document.getElementById('first');
+  let loading = 0;
+  const id = setInterval(frame, 64);
+
+  function frame() {
+    if (loading === 100) {
+      clearInterval(id);
+      console.log('holi');   
+    } else {
+      loading = loading + 1;
+      console.log('todavía no');     
+      if (loading === 90) {
+        preload.style.animation = 'fadeout 1s ease';
+      }
+    }
+  }
+}
--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,19 +1,57 @@
-function findMe() {
-  let output = document.getElementById('map');
-  if (navigator.geolocation) {
-    output.innerHTML = 'Tu navegador soporta geolocalización';
-  } else {
-    output.innerHTML = 'Tu navegador no soporta geolocalización';
-  }
-
-  function localization(position) {
-    let latitude = position.coords.latitude;
-    let longitude = position.coords.longitude;
-
-    output.innerHTML = `Latitud es ${latitude} y Longitud es ${longitude}`;
-  }
-  function error() {
-    console.log('No se pudo obtener tu localización');
-  }
-  navigator.geolocation.getCurrentPosition(localization, error);
+window.onload = () => {
+  initMap();
 };
+
+let map;
+let infowindow;
+
+// Iniciar el mapa con la localización del dispositivo
+function initMap() {
+  navigator.geolocation.getCurrentPosition(function(position) {
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
+
+    const myLatlng = new google.maps.LatLng(latitude, longitude);
+
+    const mapOptions = {
+      center: myLatlng,
+      zoom: 14,
+      mapTypeId: google.maps.MapTypeId.MAP
+    };
+
+    map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+    infowindow = new google.maps.InfoWindow();
+
+    // Para hacer la búsqueda de acuerdo a la ubicación y tipo de local
+    const request = {
+      location: myLatlng,
+      radius: 5000,
+      type: ['restaurant']
+    };
+
+    // Petición de búsqueda de los lugares con los parámetros ya indicados
+    const service = new google.maps.places.PlacesService(map);
+    service.nearbySearch(request, function(results, status) {
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+          createMarker(results[i]);
+        }
+      }
+    });
+  });
+}
+
+// Crea marcadores dentro del mapa
+function createMarker(place) {
+  let marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+  google.maps.event.addListener(marker, 'click', function() {
+    // Para mostrar nombre y dirección de los lugares
+    infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
+      '<br>' + place.vicinity + '</div>');
+    infowindow.open(map, this);
+  });
+}
