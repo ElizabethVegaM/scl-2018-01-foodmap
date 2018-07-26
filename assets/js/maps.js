49a91a782ec09164331f8263ae458ae3371dea60
Elizabeth Vega M <eb.vega.m@gmail.com>
2018-07-26 10:55:48 -0400

    trabajando en el modal con la información, por el momento muestra directamente en el html

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -43,19 +43,6 @@ function initMap() {
   });
 }
 
-// Crea marcadores dentro del mapa
-function createMarker(place) {
-  let marker = new google.maps.Marker({
-    map: map,
-    position: place.geometry.location
-  });
-  google.maps.event.addListener(marker, 'click', function() {
-    // Para mostrar nombre y dirección de los lugares
-    infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + place.vicinity + '</div>');
-    infowindow.open(map, this);
-  });
-}
-
 // Búsqueda de locales
 function search() {
   navigator.geolocation.getCurrentPosition(function(position) {
@@ -85,83 +72,31 @@ function search() {
   });
 };
 
-
-function createModal(place) {
-  console.log(place.name);
+// Crea marcadores dentro del mapa
+function createMarker(place) {
+  let marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+  google.maps.event.addListener(marker, 'click', function() {
+    // Para mostrar nombre y dirección de los lugares
+    infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + place.vicinity + '</div>');
+    infowindow.open(map, this);
+  });
 }
 
-/*
-// This example adds a search box to a map, using the Google Place Autocomplete
-// feature. People can enter geographical searches. The search box will return a
-// pick list containing a mix of places and predicted search terms.
-
-// This example requires the Places library. Include the libraries=places
-// parameter when you first load the API. For example:
-// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
-
-function initAutocomplete() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -33.8688, lng: 151.2195},
-    zoom: 13,
-    mapTypeId: 'roadmap'
+function createModal(place) {
+  console.log(place);
+  let marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+  google.maps.event.addListener(marker, 'click', function() {
+    // Para mostrar nombre y dirección de los lugares
+    infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + place.formatted_address + '</div>');
+    infowindow.open(map, this);
   });
 
-  // Create the search box and link it to the UI element.
-  let input = document.getElementById('searchInput');
-  var searchBox = new google.maps.places.SearchBox(input);
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
-  // Bias the SearchBox results towards current map's viewport.
-  map.addListener('bounds_changed', function() {
-    searchBox.setBounds(map.getBounds());
-  });
-
-  var markers = [];
-  // Listen for the event fired when the user selects a prediction and retrieve
-  // more details for that place.
-  searchBox.addListener('places_changed', function() {
-    var places = searchBox.getPlaces();
-
-    if (places.length === 0) {
-      return;
-    }
-
-    // Clear out the old markers.
-    markers.forEach(function(marker) {
-      marker.setMap(null);
-    });
-    markers = [];
-
-    // For each place, get the icon, name and location.
-    var bounds = new google.maps.LatLngBounds();
-    places.forEach(function(place) {
-      if (!place.geometry) {
-        console.log('Returned place contains no geometry');
-        return;
-      }
-      var icon = {
-        url: place.icon,
-        size: new google.maps.Size(71, 71),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(25, 25)
-      };
-
-      // Create a marker for each place.
-      markers.push(new google.maps.Marker({
-        map: map,
-        icon: icon,
-        title: place.name,
-        position: place.geometry.location
-      }));
-
-      if (place.geometry.viewport) {
-        // Only geocodes have viewport.
-        bounds.union(place.geometry.viewport);
-      } else {
-        bounds.extend(place.geometry.location);
-      }
-    });
-    map.fitBounds(bounds);
-  });
-} */
+  infoContainer = document.getElementById('results');
+  infoContainer.innerHTML += '<div class="restaurantContainer"><h6><strong>' + place.name + '</strong></h6><p>' + place.formatted_address + '</p><p>' + place.photos['0'].html_attributions['0'] + '</p><p>' + place.rating + '</div>';
+}
